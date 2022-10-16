f55d88e043babf7c8d13f16ac602fab233d14ee3
Girish Venkatachalam <girish@gayatri-hitech.com>
2022-10-16 21:12:14 +0530

    Amazing project works now

---
+++ html5-upload/server.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const multer = require('multer');
+const fs = require('file-system');
+
+const app = express();
+app.use(express.json());
+app.use(express.static(__dirname));
+
+
+// SET STORAGE
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname)
+    }
+})
+
+var upload = multer({ storage: storage })
+
+app.post('/uploadfile', upload.single('myFile'), (req, res, next) => {
+    const file = req.file
+    if (!file) {
+        const error = new Error('Please upload a file')
+        error.httpStatusCode = 400
+        return next(error)
+    }
+    res.send(file)
+})
+
+//Uploading multiple files
+app.post('/uploadmultiple', upload.array('myFiles', 12), (req, res, next) => {
+    const files = req.files
+    if (!files) {
+        const error = new Error('Please choose files')
+        error.httpStatusCode = 400
+        return next(error)
+    }
+    res.send(files)
+})
+
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+})
+
